02c5c11f1f5657ff30a2eec6f0a8388398f02d7b
Shun Takebayashi <shun@takebayashi.asia>
2016-07-30 11:54:33 +0900

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "slimane-tutorial-linebot",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/Slimane.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/noppoMan/Slimane.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/takebayashi/SwiftLineBot.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 8)
     ]
 )
